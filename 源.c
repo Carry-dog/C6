af0cdb34b2631a77df88bca1e653f97f95eaa7a5
小庄 <3011955799@qq.com>
2023-03-21 23:53:52 +0800

    6

--- 源.c
+++ 源.c
@@ -1,22 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-#include<stdio.h>
++ACM-define +AF8-CRT+AF8-SECURE+AF8-NO+AF8-WARNINGS 1
++ACM-include+ADw-stdio.h+AD4-
 int main(void)
-{
-	printf("Staring now:\n");
-	one_three();
-	printf("Done£¡\n");
-	return 0;
-}
-int one_three(void)
-{
-	printf("One\n");
-	two();
-	printf("three\n");
-	return 0;
-}
++AHs-
+	printf(+ACI-Staring now:+AFw-n+ACI-)+ADs-
+	one+AF8-three()+ADs-
+	printf(+ACI-Done+/wEAXA-n+ACI-)+ADs-
+	return 0+ADs-
++AH0-
+int one+AF8-three(void)
++AHs-
+	printf(+ACI-One+AFw-n+ACI-)+ADs-
+	two()+ADs-
+	printf(+ACI-three+AFw-n+ACI-)+ADs-
+	return 0+ADs-
++AH0-
 int two(void)
-{
-	printf("two\n");
-	return 0;
-}
++AHs-
+	printf(+ACI-two+AFw-n+ACI-)+ADs-
+	return 0+ADs-
++AH0-
 
