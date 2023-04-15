ce6516a1a33fffc0e8adf5f741017e5e1dbe4cc8
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-04-16 00:09:58 +0800

    564

---
+++ jiiiiiijk.c
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+#define BOOK "War and Peace"
+int main(void)
+{
+	float a= 2.33;
+	printf£¨"%d", a);
+		return 0;
+}
