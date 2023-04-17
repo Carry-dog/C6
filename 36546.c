8db6088fe54280e6aa1644710085d8b4339abdab
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-04-18 00:07:26 +0800

    135152

---
+++ 36546.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+
+	int a = 35;
+	while (a <= 70)
+	{
+		printf("ÄãÊÇÉµ±Æ£¡");
+		printf("#########");
+			a = a - 0.1;
+
+
+	}
+	printf("ÄãºÃÅåÆæ!");
+	return 0;
+}
