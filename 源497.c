dd0f4ef2c9d6d95a5cb5a7d04328a905db115f97
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-04-30 18:54:38 +0800

    68+

---
+++ 源497.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	long num;
+	long sum = 0L;
+	int status;
+	printf("Please enter an integr to be summed");
+	printf("(q to quit):");
+	status = scanf("%ld", &num);
+	while (status == 1)
+	{
+		sum = sum + num;
+		printf("Please enter next integer (q to quit):");
+		status = scanf("%ld", &num);
+	}
+	printf("Those integers sum to %ld.\n",sum);
+	return 0;
+}
