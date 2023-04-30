30e743f652ee1674c6e75efd15ae4f5acb23453f
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-05-01 00:53:51 +0800

    5991

---
+++ 源5686.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	const int NUMBER = 22;
+	int count = 1;
+	while (count <= NUMBER)
+	{
+		printf("Be my Valentine!\n");
+		count++;
+	}
+
+	return 0;
+}
---
+++ 源659.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	int a, b,c;
+	c=scanf("%d%d", &a, &b);
+	printf("%d",c);dc 
+
+	return 0;
+}
