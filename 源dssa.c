e553b471c4e1f1b732e8da10b64b2e2542616c8f
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-05-16 22:34:10 +0800

    s

---
+++ 源dssa.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	int a,b;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	printf("%d  %d", a, b);
+	return 0;
+}
