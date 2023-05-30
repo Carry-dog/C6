a86c792b263af37a6a3e7b9febcf60e7989b29a0
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-05-30 22:32:48 +0800

    1

---
+++ 源et5edthd.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include<stdio.h>
+int main(void)
+{
+	int i;char j;
+	int a[3];char b[3];
+	scanf("%c", &j);
+	printf("%c",j);
+	return 0;
+}
