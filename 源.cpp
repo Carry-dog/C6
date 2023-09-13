76f41b026b71a2f4dbab67ffdf7e200379be91a1
小庄 <118100778+Carry-dog@users.noreply.github.com>
2023-09-13 20:05:37 +0800

    2

--- 源.cpp
+++ 源.cpp
@@ -1,18 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #pragma warning(disable:6031)
 #include<stdio.h>
-#include<stdlib.h>
-int main(int argc, char* argv[])
+void add(int n)
 {
-	float x, y, a;
-	for (y = 1.5;y > -1.5;y -= 0.1) {
-		for (x = -1.5;x < 1.5;x += 0.05) {
-			a = x * x + y * y - 1;
-			putchar(a * a * a - x * x * y * y * y <= 0.0 ? '@' : ' ');
-		}
-		system("color 0c");
-		putchar('\n');
-	}printf("                  Éµ      ²»      À­       ¼¸£¡");
+	printf("%d %p\n", n,&n);
+	if (n < 4)
+		add(n + 1);
+		printf("%d %p      ", n, &n);
+}
+int main(void)
+{
 
+	add(1);
 	return 0;
 }
